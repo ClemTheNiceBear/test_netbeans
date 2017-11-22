fa294a877f53aef26874d4e703fb51c642bcfd71
clem <clementrisso@gmail.com>
2017-11-22 15:56:32 +0100

    "123 test"

---
+++ src/ffdebugtest/FFdebugTest.java
@@ -0,0 +1,37 @@
+package ffdebugtest;
+import static ffdebugtest.FFdebugCalc.findMin;
+import java.util.Scanner;
+
+public class FFdebugTest
+{
+    private int[] myArray;
+    private int myInt;
+    private boolean myBoolean;
+    private String myString;
+
+    public FFdebugTest(int[] par)
+    {
+        myArray = par;
+    }
+    
+    public static void main(String[] args)
+    {
+        FFdebugTest debug = new FFdebugTest(new int[3]);
+        FFdebugTest.getValue(debug.myArray);
+        System.out.print(FFdebugCalc.findMin(debug.myArray));
+
+        System.out.println("La plus grande valeur est : "
+                + FFdebugCalc.findMax(debug.myArray));
+        
+    }
+
+    public static void getValue(int[] array)
+    {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println("Entrer un nombre : ");
+            array[i] = scan.nextInt();
+        }
+    }
+}
